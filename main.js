5b09a2bbae6b281e7e5ff096827ea769343c6d6a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-10 10:49:09 -0500

    Solves Problem 1 4/4 tests

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ function ageToAbilities(age) {
         return `You can't drive.`
     } else if (age === 16 || age === 17) {
         return 'You can drive but not vote.'
-    } else if (age >=18 && age <= 24) {
-        return 'You can vote but not rent a car'
+    } else if (age > 17 && age < 25) {
+        return 'You can vote but not rent a car.'
     } else {
         return 'You can do pretty much anything.'
     }
